006bb8de640165fad7bffeacea2b7fdbe66e0ad7
Eudie <utkarsh.dubey.26@gmail.com>
2017-02-16 00:53:14 +0530

    trying pytorch

--- MiniFlow/graph.py
+++ MiniFlow/graph.py
@@ -1,1 +1,16 @@
-import tensorflow as tf
+import torch
+
+
+x = torch.Tensor(5, 3)
+x = torch.rand(5, 3)
+
+print(x)
+
+print(x.size())
+
+y = torch.rand(5, 3)
+
+if torch.cuda.is_available():
+    x = x.cuda()
+    y = y.cuda()
+    print(x + y)
